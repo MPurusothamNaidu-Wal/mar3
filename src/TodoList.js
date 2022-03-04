42e20b4c40402c8c405411a182829e3a1b6d4f05
MPurusothamNaidu-Wal <purusotham.m@westagilelabs.com>
2022-03-04 09:00:46 +0530

    commited successfully

--- src/TodoList.js
+++ src/TodoList.js
@@ -8,9 +8,9 @@ const TodoList = () => {
                 valuesAll.state.map((val, index) => {
                     return (
                         <div className="m-3">
-                            <h3 className="eachTODO" >{val.todo}</h3>
-                            <h4 className={val.status}>{val.status}</h4>
-                            <button  className="btn btn-warning" onClick={() => {
+                            <h3 >{val.todo}</h3>
+                            <h4 >{val.status}</h4>
+                            <button onClick={() => {
                                 valuesAll.dispatch({type:'deleteThisTodo', indexToDel:index})
                             }}>Delete</button>
                         </div>
